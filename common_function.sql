c7b307b0a8001cea45d706f776e7c4db16920e9c
bychiang <bychiang@yahoo-inc.com>
2016-07-12 20:47:49 +0000

    Fixed bug

--- common_function.sql
+++ common_function.sql
@@ -1,11 +1,10 @@
-DEFINE A_REMOVE_B(setA, setB) RETURN REMOVED {
-	A_LJ_B = JOIN A BY (user_query, query_type) LEFT,
-		      B BY (user_query, query_type);
-	filter_A_LJ_B = FILTER A_LJ_B BY
-			A::user_query IS NOT NULL AND
-			B::user_query IS NULL;
-	$REMOVED = FOREACH filter_A_LJ_B GENERATE
-			A::user_query AS user_query,
-			A::query_type AS query_type; 	
-}
-
+DEFINE A_REMOVE_B(setA, setB) RETURNS REMOVED {
+        A_LJ_B = JOIN $setA BY (user_query, query_type) LEFT,
+                      $setB BY (user_query, query_type);
+        filter_A_LJ_B = FILTER A_LJ_B BY
+                        $setA::user_query IS NOT NULL AND
+                        $setB::user_query IS NULL;
+        $REMOVED = FOREACH filter_A_LJ_B GENERATE
+                        $setA::user_query AS user_query,
+                        $setA::query_type AS query_type;
+};
